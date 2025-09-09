883643ccc0968e22a9904ef41934e10be08d2fa9
mo46-123 <xum60008@gmail.com>
2025-09-09 16:29:24 -0400

    Update NaturalNumber3.java
    加上了名字

--- projects/02-natural-number-on-string/src/NaturalNumber3.java
+++ projects/02-natural-number-on-string/src/NaturalNumber3.java
@@ -14,7 +14,7 @@ import components.naturalnumber.NaturalNumberSecondary;
  *         else the decimal number whose ordinary depiction is $this.rep]
  * </pre>
  *
- * @author Put your name here
+ * @author Michael X
  *
  */
 public class NaturalNumber3 extends NaturalNumberSecondary {
