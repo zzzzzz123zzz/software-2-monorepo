2325a6f455316551e28f63f1a94e2ec58eefaf45
zzzzzz123zzz <zizhengzhuang9@gmail.com>
2025-12-02 21:03:45 -0500

    Ajust the main method

--- projects/09-tag-cloud-generator/src/TagCloud.java
+++ projects/09-tag-cloud-generator/src/TagCloud.java
@@ -9,6 +9,7 @@ import components.simplereader.SimpleReader1L;
 import components.simplewriter.SimpleWriter;
 import components.simplewriter.SimpleWriter1L;
 import components.sortingmachine.SortingMachine;
+import components.sortingmachine.SortingMachine4;
 
 /**
  * This program is a word counter which users can input a file that contains
@@ -57,6 +58,15 @@ public final class TagCloud {
     /**
      * Returns the next word from the given line starting at the given position.
      * Separators include whitespace and punctuation.
+     *
+     * @param s
+     *            the input string
+     * @param start
+     *            the starting position in the string
+     * @param sep
+     *            the set of separator characters
+     * @return the next word found in the string starting from the given
+     *         position
      */
     private static String nextWord(String s, int start, Set<Character> sep) {
         int i = start;
@@ -76,187 +86,207 @@ public final class TagCloud {
     }
 
     /**
-     * Counts the number of times each word appears in the given file, and
-     * stores the results in the provided map. Words are defined as maximal
-     * sequences of non-separator characters, where separators include spaces
-     * and punctuation. Words are converted to lower case before counting.
+     * Builds the separator set.
      *
-     * @param fileName
-     *            the name of the file to be read; must not be null and must
-     *            reference an existing, readable file
-     * @param thelist
-     *            the map to update with words and their frequencies; must not
-     *            be null
-     * @param sortList
-     *            the list to store the words that exists in the file in
-     *            alphabetical order
-     *
-     * @requires fileName != null and thelist != null
-     * @ensures thelist contains exactly one entry for each distinct word in the
-     *          file, with its value equal to the number of times that word
-     *          occurs (in lower case) for int i: theSequence,
-     *          theSequence.entry(i).compareTo(theSequence.entry(i+1)) < 1
+     * @return a set of separator characters
      */
-    public static void counter(String fileName, Map<String, Integer> thelist,
-            SortingMachine<String> sortList) {
-        SimpleReader in = new SimpleReader1L(fileName);
-        /*
-         * Identify the separators including punctuation and space.
-         */
-        Set<Character> separators = new Set1L<>();
-        separators.add(' ');
-        separators.add(',');
-        separators.add('.');
-        separators.add('!');
-        separators.add('?');
-        separators.add('\n');
-        separators.add('\t');
-        separators.add('-');
-        separators.add('*');
-        separators.add('{');
-        separators.add('}');
-        separators.add('\'');
-        separators.add('\"');
-        separators.add('<');
-        separators.add('>');
-        separators.add('[');
-        separators.add(']');
-        /*
-         * Separate words by separators, and then count them into a Map by
-         * setting the words as keys and number of times as values.
-         */
+    private static Set<Character> buildSeparators() {
+        String sep = " \t\n\r,-.!?[]';:/(){}*\"<>";
+
+        Set<Character> s = new Set1L<>();
+        for (int i = 0; i < sep.length(); i++) {
+            s.add(sep.charAt(i));
+        }
+        return s;
+    }
+
+    /**
+     * Count words from input file into Map.
+     *
+     * @param file
+     *            the input file name
+     * @param wordMap
+     *            the map to store word counts
+     */
+    private static void countWords(String file, Map<String, Integer> wordMap) {
+
+        SimpleReader in = new SimpleReader1L(file);
+        Set<Character> separators = buildSeparators();
+
         while (!in.atEOS()) {
-            /*
-             * Read one line each time.
-             */
             String line = in.nextLine();
-            int start = 0;
-            int position = 0;
-            /*
-             * Set condition to avoid throwing out of bound exception.
-             */
-            while (position < line.length()) {
-                /*
-                 * Check if the charAt position is a separator.
-                 */
-                if (separators.contains(line.charAt(position))
-                        || position + 1 == line.length()) {
-                    String temp = "";
-                    /*
-                     * Set conditions to substring the words within the line.
-                     * Words will be turned into lower case.
-                     */
-                    if (position + 1 == line.length()
-                            && !separators.contains(line.charAt(position))) {
-                        temp = line.substring(start, position + 1)
-                                .toLowerCase();
+
+            int pos = 0;
+            while (pos < line.length()) {
+
+                String word = nextWord(line, pos, separators).toLowerCase();
+
+                if (!word.isEmpty()) {
+                    if (wordMap.hasKey(word)) {
+                        wordMap.replaceValue(word, wordMap.value(word) + 1);
                     } else {
-                        temp = line.substring(start, position).toLowerCase();
+                        wordMap.add(word, 1);
                     }
-                    /*
-                     * Set condition to avoid empty words.
-                     */
-                    if (!temp.isEmpty()) {
-                        /*
-                         * Set conditions to add counts each time a word is
-                         * found.
-                         */
-                        if (thelist.hasKey(temp)) {
-                            thelist.replaceValue(temp, thelist.value(temp) + 1);
-                        } else {
-                            sortList.add(temp);
-                            thelist.add(temp, 1);
-                        }
-                    }
-                    /*
-                     * Skip the separators to find the next word.
-                     */
-                    position++;
-                    start = position;
-                } else {
-                    /*
-                     * Increase the substring index of the end of a word until a
-                     * separator is found.
-                     */
-                    position++;
+                }
+
+                // move pos to end of current word or separator
+                while (pos < line.length()
+                        && !separators.contains(line.charAt(pos))) {
+                    pos++;
+                }
+                while (pos < line.length()
+                        && separators.contains(line.charAt(pos))) {
+                    pos++;
                 }
             }
         }
+
         in.close();
     }
 
     /**
-     * The main method of the program. Prompts the user for an input file name
-     * and an output file name. Counts the number of times each word appears in
-     * the input file, then generates an .html file with a table showing each
-     * distinct word and its count.
+     * Compute font size between 11 and 48.
+     *
+     * @param count
+     *            the word count
+     * @param min
+     *            the minimum count
+     * @param max
+     *            the maximum count
+     * @return the font size between 11 and 48
+     */
+    private static int computeFontSize(int count, int min, int max) {
+        final int minFont = 11;
+        final int maxFont = 48;
+
+        if (max == min) {
+            return minFont;
+        }
+
+        return minFont + (count - min) * (maxFont - minFont) / (max - min);
+    }
+
+    /**
+     * Print HTML header.
+     *
+     * @param out
+     *            the output writer
+     * @param file
+     *            the input file name
+     * @param n
+     *            the number of words to display
+     */
+    private static void printHeader(SimpleWriter out, String file, int n) {
+        out.println("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>");
+        out.println(
+                "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">");
+        out.println("<html xmlns=\"http://www.w3.org/1999/xhtml\">");
+        out.println("<head>");
+        out.println(
+                "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />");
+        out.println("<title>Top " + n + " words in " + file + "</title>");
+        out.println(
+                "<link href=\"http://web.cse.ohio-state.edu/software/2221/web-interfaces/tagcloud.css\""
+                        + " rel=\"stylesheet\" type=\"text/css\" />");
+        out.println(
+                "<link href=\"tagcloud.css\" rel=\"stylesheet\" type=\"text/css\" />");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h2>Top " + n + " words in " + file + "</h2>");
+        out.println("<hr />");
+        out.println("<div class=\"cdiv\">");
+        out.println("<p class=\"cbox\">");
+    }
+
+    /**
+     * Print HTML footer.
+     *
+     * @param out
+     *            the output writer
+     */
+    private static void printFooter(SimpleWriter out) {
+        out.println("</p>");
+        out.println("</div>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+
+    /**
+     * The main method that generates a tag cloud HTML file.
      *
      * @param args
      *            command line arguments (not used)
      *
      * @requires args != null
-     * @ensures an .html file is created, containing a table with each word
-     *          listed and the number of times it occurs.
+     * @ensures An XHTML file containing a tag cloud with the top N words from
+     *          the input file is created at the specified output file path.
      */
     public static void main(String[] args) {
+
         SimpleReader in = new SimpleReader1L();
         SimpleWriter out = new SimpleWriter1L();
-        /*
-         * Prompt the user for the file name that will be modified and the file
-         * name that the user want the output to be printed to.
-         */
-        out.println("Please enter the input file name: ");
-        String inputFileName = in.nextLine();
-        out.println("Please enter the output file name: ");
-        String outputFileName = in.nextLine();
-        SimpleWriter fileOut = new SimpleWriter1L(outputFileName);
-        /*
-         * Map used to store words and their existing times. Queue used to store
-         * the key of the map alphabetically
-         */
-        Map<String, Integer> wordList = new Map1L<String, Integer>();
-        SortingMachine<Map.Pair<String, Integer>> sm;
-        /*
-         * Print in .html format.
-         */
-        fileOut.println("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>");
-        fileOut.println(
-                "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">");
-        fileOut.println("<html xmlns=\"http://www.w3.org/1999/xhtml\">");
-        fileOut.println("<head>");
-        fileOut.println(
-                "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />");
-        fileOut.println("<title>" + "Word Counter" + "</title>");
-        fileOut.println("</head>");
-        fileOut.println("<body>");
-        fileOut.println("<h1>" + "Word Counted In" + "</h1>");
-        fileOut.println("<hr>");
-        fileOut.println("<h3>");
-        fileOut.println(
-                "Please check the table below to see the amount of times that each word appears in your file.");
-        fileOut.println(
-                "<table border=\"1\" cellpadding=\"5\" cellspacing=\"0\">");
-        fileOut.println("<tr><th>Word</th><th>Count</th></tr>");
-        /*
-         * Count the words from the input file.
-         */
-        counter(inputFileName, wordList, sm);
-        /*
-         * Print each word and its existing time accordingly in .html format
-         * with the order sorted in the sequence.
-         */
-        sm.changeToExtractionMode();
-        while (sm.size() > 0) {
-            Map.Pair<String, Integer> pair = wordList.remove(sm.removeFirst());
-            fileOut.println("<tr><td>" + pair.key() + "</td><td>" + pair.value()
-                    + "</td></tr>");
+
+        out.print("Input file: ");
+        String inputFile = in.nextLine();
+
+        out.print("Output file: ");
+        String outputFile = in.nextLine();
+
+        out.print("N (number of words): ");
+        int topN = Integer.parseInt(in.nextLine());
+
+        SimpleWriter fout = new SimpleWriter1L(outputFile);
+
+        Map<String, Integer> words = new Map1L<>();
+        countWords(inputFile, words);
+
+        // Sort by count first
+        SortingMachine<Map.Pair<String, Integer>> byCount = new SortingMachine4<>(
+                new CompareCount());
+
+        for (Map.Pair<String, Integer> p : words) {
+            byCount.add(p);
         }
-        fileOut.println("</table>");
-        fileOut.println("</body>");
-        fileOut.println("</html>");
+        byCount.changeToExtractionMode();
+
+        // Pick top N → sort alphabetically
+        SortingMachine<Map.Pair<String, Integer>> byAlpha = new SortingMachine4<>(
+                new CompareWords());
+
+        int k = 0;
+        while (k < topN && byCount.size() > 0) {
+            byAlpha.add(byCount.removeFirst());
+            k++;
+        }
+        byAlpha.changeToExtractionMode();
+
+        // compute min/max
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+
+        for (Map.Pair<String, Integer> p : byAlpha) {
+            int v = p.value();
+            if (v < min) {
+                min = v;
+            }
+            if (v > max) {
+                max = v;
+            }
+        }
+
+        // Output
+        printHeader(fout, inputFile, topN);
+        while (byAlpha.size() > 0) {
+            Map.Pair<String, Integer> p = byAlpha.removeFirst();
+            int f = computeFontSize(p.value(), min, max);
+
+            fout.println("<span class=\"f" + f + "\">" + p.key() + "</span>");
+        }
+
+        printFooter(fout);
+
         in.close();
         out.close();
-        fileOut.close();
+        fout.close();
     }
-
 }
